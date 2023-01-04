0213b88840559d44d9c3dd8533dc211108e33728
unknown <blenderthesky12@gmail.com>
2023-01-04 07:02:12 +0000

    second commit

--- proxmox/settings.py
+++ proxmox/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_c)nbf9$v3a8^s#k8bv8r#31f#&qx!fj#r$+4s(03*+-7jq7(z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['client.prom.cd','127.0.0.1','server.prom.cd']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 
